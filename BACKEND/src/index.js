408e3b49e4a7b2af90add1fb984f027cc7da930e
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-07-27 18:58:54 +0530

    API listener Handled.

--- BACKEND/src/index.js
+++ BACKEND/src/index.js
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+import { APP } from './app.js';
 
 dotenv.config({
     path:'./env'
@@ -7,3 +8,15 @@ dotenv.config({
 import connectDB from './db/index.js';
 
 connectDB()
+.then(()=>{
+    APP.on('error',(err)=>{
+        console.log("Server Error : ",err);
+    })
+
+    APP.listen(process.env.PORT||8000,()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log(`MongoDB Connection error.`,err);
+})
