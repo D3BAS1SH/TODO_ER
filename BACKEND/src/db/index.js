3c797bcae93bbe6581e48e4dd79f1295b8ce32a9
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-08-24 22:16:03 +0530

    Clean up DataBase connection block and logs.

--- BACKEND/src/db/index.js
+++ BACKEND/src/db/index.js
@@ -3,12 +3,7 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-        const connectionResponse = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`\n*************************************************
-            \n\tconnectionResponse
-            \n*************************************************
-            \n->${connectionResponse.connection.host}`);
-        console.log(connectionResponse.STATES);
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
     } catch (error) {
         console.log("MONGODB Connection error : ",error);
         process.exit(1)
