200ecd0ef8dc1d0146ebd868819ce7b8528efc3e
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-09-01 14:37:04 +0530

    Todo Controller Initialize

---
+++ BACKEND/src/controllers/todo.controller.js
@@ -0,0 +1,7 @@
+import { ApiError } from "../utils/ApiError";
+import { ApiResponse } from "../utils/ApiResponse";
+import { asynHandler } from "../utils/asyncHandler";
+
+const AddParentTodo = asynHandler(async(req,res)=>{
+    
+})
