4dc0c2fdcc79967f3d1788617e58bed3ab225b43
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-09-11 22:14:53 +0530

    Delete Function Added.

--- BACKEND/src/utils/Cloudinary.js
+++ BACKEND/src/utils/Cloudinary.js
@@ -7,9 +7,19 @@ v2.config({
     api_secret:process.env.API_SECRET
 })
 
-const uploadOnCloudinary = async(localFilePath) =>{
+const extractPubliId = (URL)=>{
+    const splited = String(URL).split('/');
+    const fileName = splited[splited.length-1];
+    return fileName.split(".")[0]
+}
+
+const uploadOnCloudinary = async(localFilePath,PresentURL=null) =>{
     try {
         if(!localFilePath) return null
+
+        if(PresentURL){
+            const DeleteResponse = await deleteFromCloudinary(PresentURL)
+        }
 
         const UploadResponse = await v2.uploader.upload(localFilePath,{
             resource_type:'auto'
@@ -24,8 +34,20 @@ const uploadOnCloudinary = async(localFilePath) =>{
     }
 }
 
-const deleteFromCloudinary = async()=>{
-    
+const deleteFromCloudinary = async(FullURL)=>{
+    try {
+        if (!FullURL){
+            return null
+        }
+
+        const DeleteResponse = await v2.uploader.destroy(extractPubliId(FullURL))
+        console.log("Deletion success");
+        console.log(DeleteResponse);
+        
+        return DeleteResponse
+    } catch (error) {
+        return {error,message:"Something went wrong while deleting."}
+    }
 }
 
 export {uploadOnCloudinary}
