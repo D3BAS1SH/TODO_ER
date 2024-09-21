1e2f2a411104e446b06e084f5e1f9fefc703f863
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-09-21 19:42:43 +0530

    Update and Delete todo controller assigned to the Subtodo router.

--- BACKEND/src/routes/SubTodo.routes.js
+++ BACKEND/src/routes/SubTodo.routes.js
@@ -1,10 +1,12 @@
 import {Router} from "express";
 import {verifyJWT} from "../middlewares/auth.middleware.js";
-import {AddSubTodo} from "../controllers/subtodo.controlller.js";
+import {AddSubTodo,DeleteSubtodo,UpdateSubTodo} from "../controllers/subtodo.controlller.js";
 
 const SubTodoRouter = Router()
 
 //secure routes
 SubTodoRouter.route('/add-subtodo/:todoId').post(verifyJWT,AddSubTodo);
+SubTodoRouter.route('/update-subtodo/:subtodoid').patch(verifyJWT,UpdateSubTodo);
+SubTodoRouter.route('/delete-subtodo/:subtodoid').delete(verifyJWT,DeleteSubtodo);
 
 export default SubTodoRouter
