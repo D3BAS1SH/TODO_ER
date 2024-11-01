c489d7df5d09b4f9c1af4ff1885e706552a71d16
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-11-01 17:39:13 +0530

    Todo Services are handled.

---
+++ FRONTEND/src/services/todo.service.js
@@ -0,0 +1,142 @@
+import axios from "axios";
+import { Store } from "../stores";
+import UserAuthService from "./auth.service.js";
+import {
+    deleteTodo,
+    selecteTodo,
+    setAddTodo,
+    setError,
+    setLoading,
+    setSelectedTodo,
+    setTodo,
+    updateTodo
+} from '../stores/todo.slice.js';
+
+class TodoService{
+    constructor(){
+        this.httpClient = axios.create({
+            baseURL:"http://localhost:8088/api/v1/todos/",
+            withCredentials:true,
+            headers:{
+                'Content-Type':"application/json"
+            }
+        });
+
+        this.setupInterceptor();
+    }
+
+    //Request Intereptor
+    setupInterceptor(){
+        this.httpClient.interceptors.request.use(
+            (config)=>{
+                Store.dispatch(setLoading(true));
+                return config
+            },
+            (error)=>{
+                Store.dispatch(setLoading(false));
+                return Promise.reject(error);
+            }
+        )
+
+        this.httpClient.interceptors.response.use(
+            (response)=>{
+                Store.dispatch(setLoading(false))
+                return response
+            },
+            async (error)=>{
+                const originalRequest = error.config;
+                if(error.response?.status===401 && !originalRequest._retry){
+                    originalRequest._retry=true;
+
+                    try {
+                        await UserAuthService.getRefreshTokens();
+                        return this.httpClient(originalRequest)
+                    } catch (error) {
+                        await UserAuthService.logout();
+                        throw error;
+                    }
+                }
+
+                Store.dispatch(setLoading(false));
+                return Promise.reject(error);
+            }
+        );
+    }
+
+    async getAllTodos(page=1,limit=5){
+        try {
+            const response = await this.httpClient.get(`/get-all-todo/`,{params:{
+                page,
+                limit
+            }});
+            Store.dispatch(setTodo(response.data));
+            return response.data;
+        } catch (error) {
+            const handledError=this.handleError(error);
+            Store.dispatch(setError(handledError.message));
+            throw handledError
+        }
+    }
+
+    async getTodoById(todoId){
+
+        try {
+            const response = await this.httpClient.get(`/get-a-todo/${todoId}`)
+            Store.dispatch(setSelectedTodo(response.data))
+            return response.data
+        } catch (error) {
+            const handledError = this.handleError(error);
+            Store.dispatch(setError(handledError.message));
+            return handledError
+        }
+    }
+    
+    async createTodo(data){
+        
+        try {
+            const response = await this.httpClient.post('/create-todo',data)
+            Store.dispatch(setAddTodo(response.data));
+            return response.data
+        } catch (error) {
+            const handledError = this.handleError(error)
+            Store.dispatch(setError(handledError.message))
+            throw handledError
+        }
+    }
+    
+    async updateTodo(todoId){
+        
+        try {
+            const response = await this.httpClient.patch(`/update-todo/${todoId}`)
+            Store.dispatch(updateTodo(response.data));
+            return response.data;
+        } catch (error) {
+            const handledError=this.handleError(error);
+            Store.dispatch(setError(handledError.message))
+            return handledError;
+        }
+    }
+    
+    async deleteaTodo(todoId){
+        
+        try {
+            const response = await this.httpClient.delete(`/delete-a-todo/${todoId}`);
+            Store.dispatch(deleteTodo(response.data));
+            return response.data
+        } catch (error) {
+            const handledError=this.handleError(error);
+            Store.dispatch(setError(handledError.message));
+            return handledError;
+        }
+    }
+
+    handleError(error){
+        if(error.response){
+            return new Error(error.response.data.message || "Server Error")
+        }
+        if(error.request){
+            return new Error(error.request.data.message||"No response from the server");
+        }
+        return new Error('Request failed');
+    }
+}
