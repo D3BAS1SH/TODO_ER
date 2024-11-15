10ad475a7555f0a5f51601b9eee4af09019adafc
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-11-15 14:50:00 +0530

    Redux Store configured.

--- FRONTEND/src/stores/index.js
+++ FRONTEND/src/stores/index.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import AuthReducer from "./auth.slice.js";
+import {todoReducer} from "./todo.slice.js";
+import {subtodoReducer} from "./subtodo.slice.js";
 
 export const Store = configureStore({
     reducer:{
-        auth:AuthReducer
+        auth:AuthReducer,
+        todo:todoReducer,
+        subTodo:subtodoReducer
     }
 })
