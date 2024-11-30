c79e976627ad22ac13bef33a0e19fbeec6c630d9
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-12-01 01:58:29 +0530

    Tailwind Font setup.

--- FRONTEND/tailwind.config.js
+++ FRONTEND/tailwind.config.js
@@ -10,7 +10,12 @@ module.exports = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        Lora:["Lora", "serif"],
+        Explora:["Explora", "serif"]
+      }
+    },
   },
   darkMode: "class",
   plugins: [nextui()]
