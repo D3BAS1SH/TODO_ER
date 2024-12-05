45bc1bea17d62fa91944a7ba7c8330ab895302fb
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-12-05 14:57:56 +0530

    Redux Store configured only for Auth slice.

--- FRONTEND/src/stores/index.js
+++ FRONTEND/src/stores/index.js
@@ -1,12 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import AuthReducer from "./auth.slice.js";
-import {todoReducer} from "./todo.slice.js";
-import {subtodoReducer} from "./subtodo.slice.js";
+import authReducer from "./auth.slice.js";
 
 export const Store = configureStore({
     reducer:{
-        auth:AuthReducer,
-        todo:todoReducer,
-        subTodo:subtodoReducer
-    }
+        auth:authReducer,
+    },
 })
