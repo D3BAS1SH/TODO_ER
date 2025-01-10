b0f33b88c8bc53b364e78b951ccf8812c29459cc
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-01-11 00:41:01 +0530

    Some changes of file name and issues.

--- BACKEND/src/routes/SubTodo.routes.js
+++ BACKEND/src/routes/SubTodo.routes.js
@@ -1,6 +1,6 @@
 import {Router} from "express";
 import {verifyJWT} from "../middlewares/auth.middleware.js";
-import {AddSubTodo,DeleteSubtodo,UpdateSubTodo,BatchAddSubtodos,BatchUpdateSubtodos} from "../controllers/subtodo.controlller.js";
+import {AddSubTodo,DeleteSubtodo,UpdateSubTodo,BatchAddSubtodos,BatchUpdateSubtodos} from "../controllers/subtodo.controller.js";
 
 const SubTodoRouter = Router()
 
