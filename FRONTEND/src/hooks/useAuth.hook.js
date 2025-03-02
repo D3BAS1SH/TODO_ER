8102e37ced1f5bc17980e0efc2ce51ecce6ddfa2
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-03-02 20:58:59 +0530

    useAuth Hook cleaned up and states from selector are separated.

--- FRONTEND/src/hooks/useAuth.hook.js
+++ FRONTEND/src/hooks/useAuth.hook.js
@@ -3,19 +3,33 @@ import {RegisterUser,LoginOut,LoginUser} from "../stores/auth.slice.js";
 
 export const useAuth = () =>{
     const dispatch = useDispatch();
-    const {user,isAuthenticated,loading,error} = useSelector((state)=>state.auth);
-
     const login = (credential) => dispatch(LoginUser(credential));
     const logout= () => dispatch(LoginOut());
     const register = (userData) => dispatch(RegisterUser(userData));
 
     return {
-        user,
-        isAuthenticated,
-        loading,
-        error,
         login,
         logout,
         register
     }
+}
+
+export const useAuthIsAuthentic = () =>{
+    const {isAuthenticated} = useSelector((state)=>state.auth);
+    return {isAuthenticated};
+}
+
+export const useAuthIsLoading = () =>{
+    const {loading} = useSelector((state)=>state.auth);
+    return {loading};
+}
+
+export const useAuthError = () =>{
+    const {error} = useSelector((state)=>state.auth);
+    return {error};
+}
+
+export const useAuthUserData = () => {
+    const {user} = useSelector((state)=>state.auth);
+    return {user};
 }
