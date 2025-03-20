402f8939909e50bedfa7089bc764d8548a18f6d2
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-03-20 16:55:50 +0530

    Todo and Subtodo component implemented in dashboard.

--- FRONTEND/src/pages/DashboardPage.jsx
+++ FRONTEND/src/pages/DashboardPage.jsx
@@ -1,8 +1,22 @@
+import Sidebar from "../components/Sidebar"
+import { useState } from "react"
+import Todos from "../components/Todos"
+import Subtodos from "../components/Subtodos"
 
 const DashboardPage = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen)
+  }
   return (
-    <div>
-      <h1>DASHboardPAGE</h1>
+    <div className="grid grid-cols-12 h-full gap-2">
+      <div className="col-span-2 h-full overflow-hidden">
+        <Todos/>
+      </div>
+      <div className="col-span-10 h- overflow-hidden">
+        <Subtodos/>
+      </div>
     </div>
   )
 }
