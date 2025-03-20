f764e20f23fb7d5d8d711998a61b858ca1673810
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-03-20 16:55:08 +0530

    Todo and Subtodo component added.

---
+++ FRONTEND/src/components/Subtodos.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { ScrollShadow } from '@heroui/scroll-shadow'
+
+const Subtodos = () => {
+  return (
+    <ScrollShadow className='w-full h-full overflow-y-auto p-6'>
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, consectetur dolorum. Consectetur suscipit ratione praesentium voluptatum sapiente ducimus dolorem, dolores cumque accusamus animi ad. A provident cum nisi impedit inventore!
+        </p>
+    </ScrollShadow>
+  )
+}
+
+export default Subtodos
---
+++ FRONTEND/src/components/Todos.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { ScrollShadow } from '@heroui/scroll-shadow'
+
+const Todos = () => {
+  return (
+    <ScrollShadow className='w-full h-full overflow-y-auto p-6'>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
+        </p>
+        
+    </ScrollShadow>
+  )
+}
+
+export default Todos
