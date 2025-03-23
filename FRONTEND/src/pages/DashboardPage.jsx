759069f9a7995c42b1cd52e957ee8e7f1d115fdc
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-03-23 22:57:06 +0530

    Changes added As Todo component and Todo Header.

--- FRONTEND/src/pages/DashboardPage.jsx
+++ FRONTEND/src/pages/DashboardPage.jsx
@@ -1,17 +1,14 @@
-import Sidebar from "../components/Sidebar"
 import { useState } from "react"
 import Todos from "../components/Todos"
 import Subtodos from "../components/Subtodos"
+import TodoHeader from "../components/TodoHeader"
 
 const DashboardPage = () => {
-  const [sidebarOpen, setSidebarOpen] = useState(false)
 
-  const toggleSidebar = () => {
-    setSidebarOpen(!sidebarOpen)
-  }
   return (
     <div className="grid grid-cols-12 h-full gap-2">
       <div className="col-span-2 h-full overflow-hidden">
+        <TodoHeader/>
         <Todos/>
       </div>
       <div className="col-span-10 h- overflow-hidden">
