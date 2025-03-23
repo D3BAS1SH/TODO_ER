effe1803dc39b833fee4da351515fe30979875eb
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-03-23 19:01:03 +0530

    RefreshToken verification added.

--- BACKEND/src/middlewares/auth.middleware.js
+++ BACKEND/src/middlewares/auth.middleware.js
@@ -5,24 +5,51 @@ import jwt from "jsonwebtoken"
 
 export const verifyJWT = asynHandler(async(req,res,next)=>{
     try {
-        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
-    
+        console.log("Verification middleware")
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")        
         if(!token){
             throw new ApiError(401,"Unauthorized request")
         }
-    
+
         const decodedToken = await jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
-    
-        const Obtaineduser = await User.findById(decodedToken?._id).select("-password -refreshToken")
-    
+        const Obtaineduser = await User.findById(decodedToken?._id).select("-password -refreshToken")        
+
         if(!Obtaineduser){
             throw new ApiError(401,"Invalid Access Token")
         }
+
+        req.user = Obtaineduser;
+        next()
+        
+    } catch (error) {
+        console.log("AccessToken error Log");
+        console.log(error);
+        throw new ApiError(401,error?.message || "invalid access token.")
+    }
+
+})
+
+export const refreshTokenVerify = asynHandler(async (req,res,next)=>{
+    try {
+        const incomingRefreshToken = req.cookies?.refreshToken || req.header("Authorization")?.replace("Bearer ","");
+        
+        if(!incomingRefreshToken){
+            throw new ApiError(401,"No token found.");
+        }
+
+        const decodedRefreshToken = await jwt.verify(incomingRefreshToken,process.env.REFRESH_TOKEN_SECRET);
+        const Obtaineduser = await User.findById(decodedRefreshToken?._id).select('-password');
+        
+        if(incomingRefreshToken!==Obtaineduser?.refreshToken){
+            throw new ApiError(401,"Invalid RefreshToken ");
+        }
+
         req.user = Obtaineduser;
         next()
 
     } catch (error) {
-        throw new ApiError(401,error?.message || "invalid access token.")
+        console.log("Logging Error");
+        console.log(error);
+        throw new ApiError(401,error?.message || "Verification Error")
     }
-
 })
