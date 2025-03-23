a63afcfb50de9e78a8bcbd451fd03334a6301410
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-03-23 22:56:35 +0530

    Todo Component is updated with latest TodoItem component as child component.

--- FRONTEND/src/components/Todos.jsx
+++ FRONTEND/src/components/Todos.jsx
@@ -1,29 +1,11 @@
 import React from 'react'
 import { ScrollShadow } from '@heroui/scroll-shadow'
+import TodoItem from './TodoItem'
 
 const Todos = () => {
   return (
     <ScrollShadow className='w-full h-full overflow-y-auto p-6'>
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates odit et explicabo suscipit pariatur alias cum quod vitae? Eius ea suscipit officia? Voluptatem eos iusto praesentium hic totam facere minus!
-        </p>
-        
+      <TodoItem/>
     </ScrollShadow>
   )
 }
