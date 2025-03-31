da1213a5484f2b30a4d6df81557333017a5b057c
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-04-01 00:12:09 +0530

    Toast Provider Wrapped on the dashboard page.

--- FRONTEND/src/pages/DashboardPage.jsx
+++ FRONTEND/src/pages/DashboardPage.jsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import Todos from "../components/Todos"
 import Subtodos from "../components/Subtodos"
 import TodoHeader from "../components/TodoHeader"
 import {useTodo} from "../hooks/useTodo.hook";
+import { ToastProvider } from "../contexts/ToastContext";
+import { Toaster } from "react-hot-toast";
 
 const DashboardPage = () => {
 
@@ -13,17 +15,28 @@ const DashboardPage = () => {
   },[])
 
   return (
-    <div className="grid grid-cols-12 h-full gap-2">
-      <div className="col-span-2 h-full flex flex-col overflow-hidden">
-        <TodoHeader/>
-        <div className="flex-1 overflow-hidden">
-          <Todos/>
+    <ToastProvider>
+      <div className="grid grid-cols-12 h-full gap-2">
+        <div className="col-span-2 h-full flex flex-col overflow-hidden">
+          <TodoHeader/>
+          <div className="flex-1 overflow-hidden">
+            <Todos/>
+          </div>
+        </div>
+        <div className="col-span-10 overflow-hidden">
+          <Subtodos/>
         </div>
       </div>
-      <div className="col-span-10 overflow-hidden">
-        <Subtodos/>
-      </div>
-    </div>
+      <Toaster position="bottom-right" toastOptions={{
+        style: {
+          zIndex: 9999,
+          background: "#333",
+          color: "#fff",
+          pointerEvents: 'all'
+        },
+        duration: 5000
+      }}/>
+    </ToastProvider>
   )
 }
 
