9fa735386b8c3b1c331d7ac31c949b2f8dac472b
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-04-01 21:51:26 +0530

    Todo Item updation and Modal for updating value assigned.

--- FRONTEND/src/components/TodoItem.jsx
+++ FRONTEND/src/components/TodoItem.jsx
@@ -1,12 +1,26 @@
-import { Checkbox } from '@heroui/react'
-import { Trash2 } from 'lucide-react';
-import React from 'react'
+import { 
+    Checkbox,
+    Modal,
+    ModalBody,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    useDisclosure,
+    Button,
+    Input
+} from '@heroui/react';
+import { Trash2, FilePenLine } from 'lucide-react';
+import React, { useState } from 'react'
 import { useTodo, useTodoAllTodo } from '../hooks/useTodo.hook';
 import { useToast } from '../contexts/ToastContext';
 
 const TodoItem = () => {
     
-    const {deleteTodoDispatcher} = useTodo();
+    const {onClose,onOpen,isOpen} = useDisclosure();
+    const [itemState,setItemState] = useState({ Heading:"", Color:"", Completed:"" });
+    const [toEdit,setToEdit] = useState(null);
+
+    const {deleteTodoDispatcher,updateTodoDispatcher} = useTodo();
     const {todos} = useTodoAllTodo();
     const { showToast } = useToast();
 
@@ -22,21 +36,66 @@ const TodoItem = () => {
         }
     }
 
+    const handleUpdateHit = async() => {
+        try {
+            const responseFromThunk = await updateTodoDispatcher({...itemState,id:toEdit._id});
+            if(responseFromThunk.type === "todo/UpdateATodo/rejected"){
+                throw new Error(responseFromThunk.payload);
+            }
+            setItemState({ Heading:"", Color:"", Completed:"" });
+            setToEdit(null);
+            showToast("Todo Updated!!","success");
+            onClose();
+        } catch (error) {
+            showToast(error?.message || "Some error happened at updation.","error");
+        }
+    }
+
+    const handleModalOpenning = (ToEdit) => {
+        setToEdit(ToEdit);
+        setItemState({
+            Heading:ToEdit.Heading,
+            Color:ToEdit.Color,
+            Completed:ToEdit.Completed
+        })
+        onOpen();
+    }
+
+    const handlerUpdateChanges = (e) => {
+        const {value,name} = e.target;
+        setItemState(item=>({
+            ...item,
+            [name]:value
+        }))
+        console.log(itemState);
+    }
+
     return (
         <div>
             {
                 todos.map(itemName=>{
-                    return <div className='flex flex-row justify-between items-center' key={itemName._id}>
+                    return (
+                    <div className='flex flex-row justify-between items-center mb-4' key={itemName._id}>
                         <Checkbox key={itemName._id} value={itemName._id} isSelected={itemName.Completed}>
                             <p style={{color:itemName.Color}} className='font-semibold'>
                                 {itemName.Heading}
                             </p>
                         </Checkbox>
-                        <div className='z-50'>
+                        <div className='flex flex-row z-50 '>
+
+                            <FilePenLine 
+                            size={'20px'}
+                            onClick={(e)=>{
+                                e.preventDefault();
+                                e.stopPropagation();
+                                handleModalOpenning(itemName);
+                            }}
+                            />
 
                             <Trash2 
                             size={'20px'}
                             onClick={(e)=>{
+                                e.preventDefault();
                                 e.stopPropagation();
                                 handleDeleteHit(itemName._id);
                             }}
@@ -44,8 +103,27 @@ const TodoItem = () => {
 
                         </div>
                     </div>
-                })
+                )})
             }
+            <Modal backdrop='blur' isOpen={isOpen} onClose={onClose} placement='auto' size='lg'>
+                <ModalContent>
+                    {
+                        (onClose)=>(
+                            <>
+                                <ModalHeader>Update Todo</ModalHeader>
+                                <ModalBody>
+                                    <Input value={itemState.Heading} name='Heading' label={'Heading'} type={'text'} variant='bordered' onChange={handlerUpdateChanges}/>
+                                    <Input value={itemState.Color} name='Color' label={'Color'} type={'color'} variant='bordered' onChange={handlerUpdateChanges}/>
+                                </ModalBody>
+                                <ModalFooter>
+                                    <Button color='warning' variant='ghost' onPress={onClose}>CANCEL</Button>
+                                    <Button color='primary' variant='ghost' onPress={handleUpdateHit}>UPDATE</Button>
+                                </ModalFooter>
+                            </>
+                        )
+                    }
+                </ModalContent>
+            </Modal>
         </div>
     )
 }
