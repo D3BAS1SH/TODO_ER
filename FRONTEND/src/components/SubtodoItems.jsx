004d886b46f9d1cd3b599514b2944e61bb8ead03
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-04-08 00:37:52 +0530

    Subtodo rendering issue fixed.

--- FRONTEND/src/components/SubtodoItems.jsx
+++ FRONTEND/src/components/SubtodoItems.jsx
@@ -1,58 +1,130 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import SubTodoSingleItem from './SubTodoSingleItem'
 import NoSubTodo from './NoSubTodo'
 import { Plus } from 'lucide-react'
-import { Input, Modal, ModalBody, ModalContent, ModalHeader, Button, ModalFooter, useDisclosure } from '@heroui/react'
+import { Input, Modal, ModalBody, ModalContent, ModalHeader, Button, ModalFooter, useDisclosure, input } from '@heroui/react'
+import { useSubTodo, useSubTodoLoading } from '../hooks/useSubTodo.hook.js'
+import { useToast } from '../contexts/ToastContext.jsx'
 
-const values = [
-    {
-        completion:true, Content:"HELLO WORLD", Color:"#f00",id:1
-    },
-    {
-        completion:false, Content:"Bye WORLD", Color:"#050",id:2
-    },
-    {
-        completion:true, Content:"HELLO WORLD", Color:"#050",id:3
-    },
-    {
-        completion:false, Content:"Bye WORLD", Color:"#050",id:4
-    },
-]
+const SubtodoItems = ({SubTodoToRender,Parent}) => {
 
-const SubtodoItems = ({SubTodoToRender}) => {
+    console.log("########Component##########");
+    console.log(SubTodoToRender,",",Parent);
+    console.log("############END############");
 
     const {isOpen,onClose,onOpen} = useDisclosure();
+    const { createSubTodoDispatcher } = useSubTodo();
+    const { showToast } = useToast();
+    const { loading } = useSubTodoLoading();
+
+    const [incomingSubtodos,setIncomingSubtodos] = useState([]);
+    const [inputState,setInputState] = useState({Content:"",Color:"#404040"})
+    const [debounceColor,setDebounceColor] = useState(inputState.Color);
+
+    useEffect(()=>{
+        setIncomingSubtodos(SubTodoToRender?.subtodos||[]);
+    },[SubTodoToRender])
+
+    const handleInputChange = (e) => {
+        e.preventDefault();
+        const {name,value} = e.target;
+
+        if(name==="Content"){
+            setInputState(prev=>({...prev,Content:value}))
+        }else if(name === "Color"){
+            setDebounceColor(value);
+        }
+        console.log(inputState);
+    }
+
+    const handleOnClick = async() => {
+        try {
+            console.log("Hitting Dispatcher to create.");
+            const responseThunk = await createSubTodoDispatcher({id:Parent,Content:inputState.Content,Color:inputState.Color,Completed:false});
+            if(responseThunk.type==="subtodo/createSubTodo/rejected"){
+                throw new Error(responseThunk.payload);
+            }
+            setInputState({Color:"#404040",Content:""});
+            setDebounceColor("#404040")
+            showToast("New Subtodo added","success");
+            onClose();
+        } catch (error) {
+            console.error(error.message||"Some error happened at creating SubTodo.");
+            showToast(error.message||"Some error happened at creating SubTodo.","error");
+        }
+    }
+
+    const handleOnCancelHit = () => {
+        setInputState((state)=>{
+            return {Color:"#404040",Content:""}
+        })
+        setDebounceColor("#404040");
+        onClose()
+    }
+
+    useEffect(()=>{
+        const timer = setTimeout(()=>{
+            setInputState(prev=>({...prev,Color:debounceColor}));
+        },200)
+
+        return ()=>clearTimeout(timer);
+    },[debounceColor])
 
     return (
-        <div className='flex flex-col gap-8 h-full w-full'>
-            {
-                (!!values.length)?
-                values.map(item=>{
-                    return <SubTodoSingleItem key={item.id} Color={item.Color} Content={item.Content} Completion={item.completion}/>
-                })
-                :
-                <NoSubTodo/>
-            }
-            <Button fullWidth={true} variant='solid' radius='sm' onPress={onOpen}>
-                <div className='flex flex-row justify-center items-center gap-3'>
-                    <Plus/>
-                    <p className='font-semibold text-[#494949]'>
-                        ADD ONE MORE
-                    </p>
-                </div>
-            </Button>
-            <Modal backdrop='blur' isOpen={isOpen} onClose={onClose} placement='center' size='lg'>
+        <div className='flex flex-col h-full w-full'>
+            <div className='flex-1 overflow-y-auto flex flex-col gap-4'>
+                {
+                    (!!incomingSubtodos.length)?
+                    incomingSubtodos.map(item=>{
+                        return <SubTodoSingleItem key={item.id} Color={item.Color} Content={item.Content} Completion={item.completion}/>
+                    })
+                    :
+                    <NoSubTodo/>
+                }
+            </div>
+            <div>
+                <Button fullWidth={true} variant='solid' radius='sm' onPress={onOpen}>
+                    <div className='flex flex-row justify-center items-center gap-3'>
+                        <Plus/>
+                        <p className='font-semibold text-[#494949]'>
+                            ADD ONE MORE
+                        </p>
+                    </div>
+                </Button>
+            </div>
+            <Modal backdrop='blur' isOpen={isOpen} onClose={onClose} placement='center' size='4xl'>
                 <ModalContent>
                     {
                         (onClose)=>(
                             <>
                                 <ModalHeader>ADD SUBTODO</ModalHeader>
                                 <ModalBody>
-                                    <Input name='Content'/>
+                                    <div className='grid grid-cols-10 gap-4'>
+                                        <div className='col-span-7 h-full w-full'>
+                                            <Input name='Content' type='text' value={inputState.Content} onChange={handleInputChange}/>
+                                        </div>
+                                        <div className='col-span-3 h-full w-full'>
+                                            <Input name="Color" type='color' value={debounceColor} onChange={handleInputChange}/>
+                                        </div>
+                                    </div>
                                 </ModalBody>
                                 <ModalFooter>
-                                    <Button variant='ghost' color='warning'>CANCEL</Button>
-                                    <Button variant='ghost' color='primary'>SUBMIT</Button>
+                                    <Button
+                                    variant='ghost'
+                                    color='warning'
+                                    onPress={handleOnCancelHit}
+                                    >
+                                        CANCEL
+                                    </Button>
+
+                                    <Button
+                                    variant='ghost'
+                                    color='primary'
+                                    onPress={handleOnClick}
+                                    isLoading={loading}
+                                    >
+                                        SUBMIT
+                                    </Button>
                                 </ModalFooter>
                             </>
                         )
--- FRONTEND/src/stores/subtodo.slice.js
+++ FRONTEND/src/stores/subtodo.slice.js
@@ -1,11 +1,30 @@
 import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
 import {getAllTodoThunk} from "./todo.slice.js"
+import subtodoService from "../services/subtodo.service.js";
 
 const initialState={
     loading:false,
     error:null,
     subtodos:[],
 }
+
+export const createSubTodoThunk = createAsyncThunk(
+    "subtodo/createSubTodo",
+    async(subTodoDoc,{rejectWithValue})=>{
+        try {
+            console.log("In Thunk");
+            if(!subTodoDoc){
+                return rejectWithValue("No Subtodo document provided.");
+            }
+            const {id,Content,Color,Completed} = subTodoDoc;
+            const response = await subtodoService.createSubTodo({todoid:id,subtodoObj:{Content,Color,Completed}});
+            return response.data;
+        } catch (error) {
+            console.log("Create Subtodo thunk failed.");
+            return rejectWithValue(error?.message || "Error on creating subtodo");
+        }
+    }
+)
 
 const subtodoSlice = createSlice(
     {
@@ -26,6 +45,27 @@ const subtodoSlice = createSlice(
             })
             .addCase(getAllTodoThunk.rejected,(state,action)=>{
                 state.error=action.error;
+                state.loading=false;
+            })
+            .addCase(createSubTodoThunk.pending,(state,_)=>{
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(createSubTodoThunk.fulfilled,(state,action)=>{
+                console.log("Payload received.");
+                console.log("Where is _id?");
+                state.loading = false;
+                state.error = null;
+                state.subtodos = state.subtodos.map(item=>{
+                    if(item._id === action.payload.data.Parent){
+                        return {...item,subtodos:[...item.subtodos,action.payload.data]}
+                    }
+                    return item;
+                })
+                console.log("Where is _id?");
+            })
+            .addCase(createSubTodoThunk.rejected,(state,action)=>{
+                state.error=action.payload;
                 state.loading=false;
             })
         }
