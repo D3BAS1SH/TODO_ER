7dc5ef2b2fd640407dfdbb8bcc6bb5ea0778d3c7
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-04-12 10:49:03 +0530

    CORS updated.

--- BACKEND/src/utils/cors.config.js
+++ BACKEND/src/utils/cors.config.js
@@ -1,8 +1,12 @@
 const corsConfig = {
-    origin: process.env.CORS_ORIGIN,
+    origin: [
+        process.env.CORS_ORIGIN_LOCAL,
+        process.env.CORS_ORIGIN_URL_V1,
+        process.env.CORS_ORIGIN_URL_V2
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS',"PATCH"],
     allowedHeaders: ['Content-Type', 'Authorization','X-Requested-With'],
 };
-  
+
 export default corsConfig;
